453721a96be65740a19ac78ece8cdff2fe4351c3
Bartosz Michalak <barmic12@gmail.com>
2018-12-13 01:11:31 +0100

    Initial commit. Add Devise & JWT to authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::API
   def api_version
     render json: {"api-version": "0.01" }
   end
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error(resource)
+    render json: {
+      errors: [
+        {
+          status: 400,
+          details: resource.errors
+        }
+      ]
+    }, status: 400
+  end
 end
